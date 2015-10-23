1cc99a6227dc99ea6d206c41a85f4a99f12193a5
Rafał Polito <rafal.polito@techland.pl>
2015-10-23 20:03:09 +0200

    Added camera shake.

--- Assets/Scripts/CameraRotator.cs
+++ Assets/Scripts/CameraRotator.cs
@@ -4,13 +4,14 @@ using System.Collections;
 public class CameraRotator : MonoBehaviour {
 
     public float m_CameraClampX = 10;
+    public float m_ShakePower = 10;
 
 	void Update () {
         float rotationX = transform.eulerAngles.x + Input.GetAxis("CameraMoveVertical");
         float rotationY = transform.eulerAngles.y + Input.GetAxis("CameraMoveHorizontal");
 
         if(rotationX < 90 - m_CameraClampX || rotationX > 270 + m_CameraClampX) {
-          transform.eulerAngles = new Vector3(rotationX, rotationY, transform.eulerAngles.z);
+          transform.eulerAngles = new Vector3(rotationX, rotationY, Input.GetAxis("CameraShake") * m_ShakePower);
         }
     }
 
