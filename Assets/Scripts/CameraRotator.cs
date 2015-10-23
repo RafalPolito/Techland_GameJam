f9ef2796e4fc376f2e5734eb706b449ceb6e9f39
Rafał Polito <rafal.polito@techland.pl>
2015-10-23 19:55:49 +0200

    Added player and rotating camera.

---
+++ Assets/Scripts/CameraRotator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRotator : MonoBehaviour {
+
+    public float m_CameraClampX = 10;
+
+	void Update () {
+        float rotationX = transform.eulerAngles.x + Input.GetAxis("CameraMoveVertical");
+        float rotationY = transform.eulerAngles.y + Input.GetAxis("CameraMoveHorizontal");
+
+        if(rotationX < 90 - m_CameraClampX || rotationX > 270 + m_CameraClampX) {
+          transform.eulerAngles = new Vector3(rotationX, rotationY, transform.eulerAngles.z);
+        }
+    }
+
+}
