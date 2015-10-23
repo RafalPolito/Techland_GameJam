5069c52b41969adcff1a8b1174a6a3ad4ef7cd77
Rafał Polito <rafal.polito@techland.pl>
2015-10-24 00:43:11 +0200

    Added title screen.

---
+++ Assets/Scripts/Hider.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Hider : MonoBehaviour {
+
+    public float m_Timer = 3;
+    private Text m_Text;
+
+	void Start () {
+        m_Text = GetComponent<Text>();
+    }
+
+    void Update() {
+        if(m_Timer <= 0) {
+            m_Text.color = Color.Lerp(m_Text.color, Color.clear, Time.deltaTime * 2);
+        } else {
+            m_Timer -= Time.deltaTime;
+        }
+    }
+	
+}
