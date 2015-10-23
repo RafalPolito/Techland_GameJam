eea183ec536922d68a962547152c18105d2d2fd2
Rafał Polito <rafal.polito@techland.pl>
2015-10-23 20:47:37 +0200

    Added camera shakle.

---
+++ Assets/SnowEmitter.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SnowEmitter : MonoBehaviour {
+
+    public ParticleSystem m_EmitterInstance;
+    public float m_LastSign = 1;
+
+    void Awake() {
+        m_EmitterInstance.Stop();
+        m_EmitterInstance.Clear();
+    }
+
+    void Update () {
+        float cameraShake = Input.GetAxis("CameraShake");
+
+        if(cameraShake != 0 && Mathf.Sign(cameraShake) != m_LastSign) {
+            //m_EmitterInstance.time = 0;
+            m_EmitterInstance.Emit(10);
+            m_LastSign = Mathf.Sign(cameraShake);
+        } else if(m_EmitterInstance != null)  {
+            m_EmitterInstance.Stop();
+        }
+	}
+}
